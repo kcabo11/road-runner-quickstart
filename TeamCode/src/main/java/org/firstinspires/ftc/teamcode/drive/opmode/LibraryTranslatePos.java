c9681841dd2b369f1009e77a1c2c10b283e0535a
Sharon <beeppatrol@gmail.com>
2020-11-14 16:47:03 -0700

    Worked on Translate library, and fixed points in auto. Not working currently

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LibraryTranslatePos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LibraryTranslatePos.java
@@ -1,20 +1,28 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class LibraryTranslatePos {
-    int startXPos = -60;
-    int startYPos = -60;
+    int startXPos = 84;
+    int startYPos = 12;
+    Telemetry telemetry;
+    public LibraryTranslatePos(Telemetry newtelemetry){
+        telemetry = newtelemetry;
+    }
 
-    public double TranslatingX(int newXPos) {
-        int Xt;
+    public double TranslatingX(double newXPos) {
+        double newX = (newXPos*24) + startXPos;
+//        int newX = 22;
+        telemetry.addData("Translating xPos",newX);
+        telemetry.update();
+        return newX;
 
-        Xt = newXPos-startXPos;
-
-        return Xt;
-
-    }public double TranslatingY(int newYPos) {
-        int Yt;
-        Yt = newYPos-startYPos;
-        return Yt;
+    }public double TranslatingY(double newYPos) {
+        double newY = (newYPos*24) + startYPos;
+//        int newY = newYPos-22;
+        telemetry.addData("Translating yPos",newY);
+        telemetry.update();
+        return newY;
     }
 }
 
