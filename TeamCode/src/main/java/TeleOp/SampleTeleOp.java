4e1eb347eb0c585c9281b01ba62af67ce8c3cfc7
Josh Wllet <oogorg@yahoo.com>
2021-01-02 15:18:53 -0700

    Joshs programs

--- TeamCode/src/main/java/TeleOp/SampleTeleOp.java
+++ TeamCode/src/main/java/TeleOp/SampleTeleOp.java
@@ -1,6 +1,5 @@
 package TeleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
