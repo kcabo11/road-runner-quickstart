7d672464150ff789c177614862c82bc49549d7a0
kcabo11 <beeppatrol@gmail.com>
2021-01-21 17:39:48 -0700

    OpenCV Lib not work for webcam, Auto is running, testing launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Automous/RedMainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Automous/RedMainAuto.java
@@ -5,7 +5,10 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.drive.opmode.LibraryOpenCV;
 
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -13,6 +16,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import Sample.SampleMecanumDrive;
@@ -22,10 +26,16 @@ import Sample.SampleMecanumDrive;
 @Autonomous(group = "Blue Side Auto")
 public class RedMainAuto extends LinearOpMode {
     static SampleMecanumDrive robot;
-//    LibraryOpenCV opencv;
+    LibraryOpenCV opencv;
+    //Telemetry telemetry;
+    //HardwareMap hardwareMap;
 //    static LibraryTranslatePos translatePos;
     String RingConfig;
 
+    private DcMotor intake;
+    private DcMotor flyWheel;
+    private Servo fireSelector;
+    private Servo intake_aid;
     private DcMotor arm;
     private DcMotor arm2;
     private Servo wobble_grabber;
@@ -33,21 +43,29 @@ public class RedMainAuto extends LinearOpMode {
     static Pose2d startingpos;
     static Pose2d myPos;
     static Pose2d myPose;
+    static Vector2d wobbleDrop;
 
     static Trajectory targetZoneDelivery;
     static Trajectory grabSecondWG;
     static Trajectory myTrajectory;
+    static Trajectory toShootRings;
 //    static Vector2d[] targetzonepath  = new Vector2d[3];
     static Vector2d grabWobbleGoal;
+    static Vector2d shootRings;
+    static Vector2d turning;
 
     public static void generatePlotPoints() {
 
-//         targetzonepath[0] = new Vector2d (84,0);//-60
-//         targetzonepath[1] = new Vector2d (108,0);//-60
-//         targetzonepath[2] = new Vector2d (132,0);//-60
+//         targetzonepath[0] = new Vector2d (60,0);//-60
+//         targetzonepath[1] = new Vector2d (84,24);//-60
+//         targetzonepath[2] = new Vector2d (110,0);//-60
 
         grabWobbleGoal = new Vector2d(12,24);
         Pose2d myPose = new Pose2d(10, -5, Math.toRadians(90));
+        shootRings = (new Vector2d(60,48));
+        turning = (new Vector2d(61,48));
+        wobbleDrop = new Vector2d(115,0);
+
 
 
     }
@@ -77,8 +95,11 @@ public class RedMainAuto extends LinearOpMode {
 //                    .build();
 
             myTrajectory = robot.trajectoryBuilder(new Pose2d(0, 0, 0))
-//                    .splineTo(new Pose2d(15, 15, 0))
-                    .lineTo(new Vector2d(120, 0))
+                    .lineTo(wobbleDrop)
+                    //.lineTo(new Vector2d(84,24))
+                    .build();
+            toShootRings = robot.trajectoryBuilder(new Pose2d(110,0,0)) //Pose2d(84,24))
+                    .splineToConstantHeading(shootRings, 0)
                     .build();
 
 //            Trajectory myTrajectory = robot.trajectoryBuilder(new Pose2d(1,0,0)).strafeRight(10).forward(5).build();
@@ -86,27 +107,33 @@ public class RedMainAuto extends LinearOpMode {
 }
     @Override
     public void runOpMode() throws InterruptedException {
+
         robot = new SampleMecanumDrive(hardwareMap);
 
         arm = hardwareMap.dcMotor.get("arm");
         arm2 = hardwareMap.dcMotor.get("arm2");
         wobble_grabber = hardwareMap.servo.get("wobble_grabber");
+        intake = hardwareMap.dcMotor.get("intake");
+        flyWheel = hardwareMap.dcMotor.get("fly_wheel");
+        fireSelector = hardwareMap.servo.get("fire_selector");
+        intake_aid = hardwareMap.servo.get("intake_aid");
         arm2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("Telemetry", "robot initializing");
         telemetry.update();
 
-//        opencv = new LibraryOpenCV(robot, telemetry, hardwareMap);
-//        opencv.initOpenCV();
+        opencv = new LibraryOpenCV(robot, telemetry, hardwareMap);
+        opencv.initOpenCV();
         telemetry.addData("OpenCV initialized", "");
         telemetry.update();
 
-//        while (!isStarted()) {
-////            RingConfig = opencv.findRingConfig();
-//            telemetry.addData("timer", getRuntime());
-//            telemetry.update();
-//        }
-//        opencv.shutDownOpenCV();
+        while (!isStarted()) {
+            RingConfig = opencv.findRingConfig();
+            telemetry.addData("opencv reading", opencv.findRingConfig());
+            telemetry.addData("timer", getRuntime());
+            telemetry.update();
+        }
+        opencv.shutDownOpenCV();
         waitForStart();
 
         telemetry.addData("RunOpMode","");
@@ -114,42 +141,40 @@ public class RedMainAuto extends LinearOpMode {
 
         generatePath(RingConfig);
 
-        robot.followTrajectory(myTrajectory);
-        clawBack(robot);
+        //robot.followTrajectory(myTrajectory);
+        //clawBack(robot);
+        //robot.followTrajectory(toShootRings);
+        //sleep(2000);
+        startLauncher(robot);
+        launchRings(robot);
+        launchRings2(robot);
+        launchRings(robot);
+        launchRings2(robot);
 
-//        if (RingConfig == "NONE"){
-//            telemetry.addData("Position A","");
-//            telemetry.update();
-//
-////            robot.followTrajectory(targetZoneDelivery);
-////            sleep(5000);
-////            robot.followTrajectory(grabSecondWG);
-//
-//        }if (RingConfig == "ONE"){
-//            telemetry.addData("Position B", "");
-//            telemetry.update();
-//
-////            robot.followTrajectory(targetZoneDelivery);
-////            sleep(5000);
-////            robot.followTrajectory(grabSecondWG);
-//
-//        }if (RingConfig == "FOUR"){
-//            telemetry.addData("Position C", "");
-//            telemetry.update();
-//
-////            robot.followTrajectory(targetZoneDelivery);
-////            sleep(5000);
-////            robot.followTrajectory(grabSecondWG);
-//        }
     }
 
-    public void clawBack (SampleMecanumDrive robot){
-        arm.setPower(1);
-        arm2.setPower(1);
-        sleep(500);
+    private void clawBack (SampleMecanumDrive robot){
+        arm.setPower(-.8);
+        arm2.setPower(-.8);
+        sleep(400);
         arm.setPower(0);
         arm2.setPower(0);
         sleep(250);
-        wobble_grabber.setPosition(0);
+        wobble_grabber.setPosition(-1);
+        sleep(200);
+        arm.setPower(.8);
+        arm2.setPower(.8);
+        sleep(500);
+        arm.setPower(0);
+        arm2.setPower(0);
+    }
+    private void startLauncher (SampleMecanumDrive robot){
+        flyWheel.setPower(1);
+    }
+    private void launchRings (SampleMecanumDrive robot){
+        fireSelector.setPosition(0);
+    }
+    private void launchRings2 (SampleMecanumDrive robot){
+        fireSelector.setPosition(1);
     }
 }
